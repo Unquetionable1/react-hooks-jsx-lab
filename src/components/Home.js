ff1dfdc0e99c91c5d0d78a8bfcea3b45b807f956
Edmound Njoori <eddnjoori@gmail.com>
2024-04-26 10:26:50 +0300

    Adds the required components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,16 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const hstyle = {
+    color: "firebrick",
+  };
+  return (
+    <div id="home">
+      <h1 style={hstyle}>
+        {name} is a Web Developer from {city}`
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
